4105af6263178ed8b6defb0338572125a7b0c868
Alexandre Alvarez Messias <ale03.alvarez@hotmail.com>
2017-10-18 22:17:07 -0200

    Adicionado Controller

---
+++ MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MeuPrimeiroBackend.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        [Required, StringLength(20)]
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int DanoMaximo { get; set; }
+
+        public int DanoMinimo { get; set; }
+
+        public int Durabilidade { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
